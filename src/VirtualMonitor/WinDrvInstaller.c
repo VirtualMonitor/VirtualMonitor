738ae31f79f24a7b2e1daca1790aabcccbd4e9ac
Derek <derek0883@gmail.com>
2013-11-04 21:20:15 -0800

    RegDeleteTree is not avalible on win2k winXp. So use RegDelnode from MSDN http://msdn.microsoft.com/en-us/library/ms724235%28VS.85%29.aspx

--- src/VirtualMonitor/WinDrvInstaller.c
+++ src/VirtualMonitor/WinDrvInstaller.c
@@ -7,6 +7,7 @@
 #include <Newdev.h>
 #include <Cfgmgr32.h>
 #include <Shlobj.h>
+#include <strsafe.h>
 
 typedef BOOL (*PINSTALL_NEW_DEVICE)(HWND, LPGUID, PDWORD);
 typedef BOOL (*PUpdateDriverForPlugAndPlayDevices)(HWND hwndParent,
@@ -656,6 +657,123 @@ out:
 	return ret;
 }
 
+
+//*************************************************************
+//  http://msdn.microsoft.com/en-us/library/ms724235%28VS.85%29.aspx
+//  RegDelnodeRecurse()
+//
+//  Purpose:    Deletes a registry key and all its subkeys / values.
+//
+//  Parameters: hKeyRoot    -   Root key
+//              lpSubKey    -   SubKey to delete
+//
+//  Return:     TRUE if successful.
+//              FALSE if an error occurs.
+//
+//*************************************************************
+BOOL RegDelnodeRecurse (HKEY hKeyRoot, LPTSTR lpSubKey)
+{
+    LPTSTR lpEnd;
+    LONG lResult;
+    DWORD dwSize;
+    TCHAR szName[MAX_PATH];
+    HKEY hKey;
+    FILETIME ftWrite;
+
+    // First, see if we can delete the key without having
+    // to recurse.
+
+    lResult = RegDeleteKey(hKeyRoot, lpSubKey);
+
+    if (lResult == ERROR_SUCCESS) 
+        return TRUE;
+
+    lResult = RegOpenKeyEx (hKeyRoot, lpSubKey, 0, KEY_READ, &hKey);
+
+    if (lResult != ERROR_SUCCESS) 
+    {
+        if (lResult == ERROR_FILE_NOT_FOUND) {
+            printf("Key not found.\n");
+            return TRUE;
+        } 
+        else {
+            printf("Error opening key.\n");
+            return FALSE;
+        }
+    }
+
+    // Check for an ending slash and add one if it is missing.
+
+    lpEnd = lpSubKey + lstrlen(lpSubKey);
+
+    if (*(lpEnd - 1) != TEXT('\\')) 
+    {
+        *lpEnd =  TEXT('\\');
+        lpEnd++;
+        *lpEnd =  TEXT('\0');
+    }
+
+    // Enumerate the keys
+
+    dwSize = MAX_PATH;
+    lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
+                           NULL, NULL, &ftWrite);
+
+    if (lResult == ERROR_SUCCESS) 
+    {
+        do {
+
+            StringCchCopy (lpEnd, MAX_PATH*2, szName);
+
+            if (!RegDelnodeRecurse(hKeyRoot, lpSubKey)) {
+                break;
+            }
+
+            dwSize = MAX_PATH;
+
+            lResult = RegEnumKeyEx(hKey, 0, szName, &dwSize, NULL,
+                                   NULL, NULL, &ftWrite);
+
+        } while (lResult == ERROR_SUCCESS);
+    }
+
+    lpEnd--;
+    *lpEnd = TEXT('\0');
+
+    RegCloseKey (hKey);
+
+    // Try again to delete the key.
+
+    lResult = RegDeleteKey(hKeyRoot, lpSubKey);
+
+    if (lResult == ERROR_SUCCESS) 
+        return TRUE;
+
+    return FALSE;
+}
+
+//*************************************************************
+//  http://msdn.microsoft.com/en-us/library/ms724235%28VS.85%29.aspx
+//  RegDelnode()
+//
+//  Purpose:    Deletes a registry key and all its subkeys / values.
+//
+//  Parameters: hKeyRoot    -   Root key
+//              lpSubKey    -   SubKey to delete
+//
+//  Return:     TRUE if successful.
+//              FALSE if an error occurs.
+//
+//*************************************************************
+
+BOOL RegDelnode (HKEY hKeyRoot, LPTSTR lpSubKey)
+{
+    TCHAR szDelKey[MAX_PATH*2];
+
+    StringCchCopy (szDelKey, MAX_PATH*2, lpSubKey);
+    return RegDelnodeRecurse(hKeyRoot, szDelKey);
+}
+
 BOOL RegClean()
 {
     HKEY hRegRoot;
@@ -669,6 +787,7 @@ BOOL RegClean()
     DWORD dwType = 0;
     CHAR lpDesc[256]; 
     DWORD dwSize= sizeof(lpDesc);
+	BOOL res = FALSE;
 
     rc = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
 						"SYSTEM\\CurrentControlSet\\Control\\Video",
@@ -702,7 +821,7 @@ BOOL RegClean()
                             &hSubKey);
         if (rc != ERROR_SUCCESS) {
             logError("RegClean Open Sub Key Failed: %x, %d\n", GetLastError(), rc);
-				continue;
+			continue;
         }
         rc = RegQueryValueEx(hSubKey,
                 "Service",
@@ -722,8 +841,10 @@ BOOL RegClean()
             if (stricmp(lpDesc, DRIVER_NAME) == 0) {
         		RegCloseKey(hSubKey);
 				hSubKey = NULL;
-				rc = RegDeleteTree(hRegRoot, idName);
-				logInfo("RegClean %s %x %d\n", idName, GetLastError(), rc);
+				// RegDeleteTree is not avalible on win2k winXp
+				// rc = RegDeleteTree(hRegRoot, idName);
+				res = RegDelnode(hRegRoot, idName);
+				logInfo("RegClean %s %x %d\n", idName, GetLastError(), res);
 			}
         }
 		if (hSubKey) {
